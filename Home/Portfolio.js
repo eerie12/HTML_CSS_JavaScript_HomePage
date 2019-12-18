fbb4e3ea384bed9bccd2a3b4ab8b155361cf2c47
eerie12 <eerie202@gmail.com>
2019-12-19 02:49:01 +0900

    HTML_CSS_JavaScriptでHP制作

---
+++ Home/Portfolio.js
@@ -0,0 +1,79 @@
+/*HEADER*/ 
+window.onload = function(){scrollFunction()};
+window.onscroll = function(){scrollFunction()}
+
+function scrollFunction(){
+    var header = document.getElementById('header');
+
+    if(document.documentElement.scrollTop > 70){
+        if(!header.classList.contains('navbar-fixed')){
+            header.classList.add('navbar-fixed');
+            document.getElementsByTagName('bodt')[0].style.marginTop = '70px';
+            header.style.display = 'none';
+            setTimeout(function(){
+                header.style.display = 'block';
+            },40);
+        }
+    } else{
+        if(header.classList.contains('navbar-fixed')){
+            header.classList.remove('navbar-fixed');
+            document.getElementsByTagName('body')[0].style.marginTop = '0';
+        }
+    }
+
+}
+
+function menuToggle(){
+    document.getElementById('menu').classList.toggle('show');
+
+}
+document.getElementById('toggleBtn').addEventListener('click',menuToggle);
+
+/*area*/
+var imageSlideIndex = 1;
+showImageSlides(imageSlideIndex);
+
+function imageSlideTimer(){
+    plusImageSlides(1);
+}
+
+var imageTimer = setInterval(imageSlideTimer,3000);
+
+function plusImageSlides(n){
+    clearInterval(imageTimer);//intervalをクリアした後再びinterval->使用者の命令が無視されない。
+    imageTimer = setInterval(imageSlideTimer,3000);
+
+    showImageSlides(imageSlideIndex += n);
+}
+
+function currentImageSlides(n){
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer,3000);
+
+    showImageSlides(imageSlideIndex = n);
+}
+
+function showImageSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('image-slide');
+    var dots   = document.getElementsByClassName('dot');
+    if(n > slides.length){imageSlideIndex = 1}
+    if(n < 1 ){imageSlideIndex = slides.length}
+    for(i = 0; i<slides.length;i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i< dots.length;i++){
+        dots[i].className = dots[i].className.replace(' active', '');        
+    }
+    slides[imageSlideIndex - 1].style.display = 'block';
+    dots[imageSlideIndex - 1].className += ' active';
+
+}
+
+document.getElementById('imagePrev').addEventListener('click',plusImageSlides.bind(null,-1));
+document.getElementById('imageNext').addEventListener('click',plusImageSlides.bind(null,1));
+
+document.getElementById('firstDot').addEventListener('click', currentImageSlides.bind(null,1));
+document.getElementById('secondDot').addEventListener('click', currentImageSlides.bind(null,2));
+document.getElementById('thirdDot').addEventListener('click', currentImageSlides.bind(null,3));
+document.getElementById('fourthDot').addEventListener('click', currentImageSlides.bind(null,4));
